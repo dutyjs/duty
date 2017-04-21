e6e34a71769639c06ba56386d41893f5fea9323b
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-04-21 08:51:53 +0100

    a class to handle platform differencies
    This class checks to see if a file exists in a given platform

---
+++ platform.js
@@ -0,0 +1,27 @@
+
+const fs = require('fs');
+
+class Platform {
+    
+    constructor() {}
+    
+    static createType() {
+        return new Platform();
+    }
+
+    checkPlatform(platform) {
+        return this[platform]();
+    }
+    
+    linux() {
+        
+        if (
+            fs.existsSync('/etc/systemd/system/duty-js.service')
+                
+        ) {
+            return true;
+        }
+    }
+}
+
+module.exports = Platform;
