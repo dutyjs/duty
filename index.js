d05f56ef0d902411520b059e5eefaa1a86150fd0
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-05-14 09:08:12 +0100

    add notifcation and timeout support to config.json, and also fix a bug
    The bug requires duty.js in the src folder ( which does not exists ) of the current working directory

--- index.js
+++ index.js
@@ -4,91 +4,94 @@ const mkdirp = require('mkdirp');
 
 try {
 
-    const config = require('./config.json');
+	const config = require('./config.json');
 
-    const config_locationContent = require(config.location);
+	const config_locationContent = require(config.location);
 
-    let { location } = config;
+	let { location } = config;
 
-    module.exports = require('./duty.js')({config_locationContent,location});
+	module.exports = require('./duty.js')({config_locationContent,location});
 
 
 } catch(ex) {
 
-    ; ( () => {
-	const rl = require('readline');
-	const DEFAULT = `${process.env.HOME}/.duty.json`;
-	const interface = rl.createInterface({
-	    input: process.stdin,
-	    output: process.stdout
-	});
+	; ( () => {
+		const rl = require('readline');
+		const DEFAULT = `${process.env.HOME}/.duty.json`;
+		const notification = true;
+		const timeout = 60000;
 
-	function askQuestion(question) {
-	    return new Promise((resolve) => {
-		interface.question(question, answer => resolve(answer) );
-	    });
-	}
+		const interface = rl.createInterface({
+			input: process.stdin,
+			output: process.stdout
+		});
 
-	async function _f() {
-
-	    try {
-
-		let answer = await askQuestion(
-		    `default todo location not found, use \"${DEFAULT}\" as default (y/n) `
-		), obj;
-
-		switch(answer) {
-		case 'y':
-
-		    interface.close();
-
-		    obj = { location: DEFAULT};
-
-		    fs.writeFileSync(DEFAULT, "{}");
-
-		    fs.writeFileSync("./config.json", JSON.stringify(obj));
-
-		    module.exports = require('./src/duty.js')(DEFAULT);
-
-		    break;
-		case 'n':
-
-		    answer = await askQuestion(
-			`specify a location to save the information: `
-		    );
-
-		    interface.close();
-
-		    if ( answer.length === 0 ) {
-			console.error(`invalid input specified\n`);
-			return ;
-		    } else {
-
-			mkdirp.sync(answer.substr(0,answer.lastIndexOf(path.sep)));
-
-			obj = { location: answer + ".json"};
-
-			fs.writeFileSync(obj.location, "{}");
-
-			fs.writeFileSync("./config.json", JSON.stringify(obj));
-
-			module.exports = require('./src/duty.js')(answer);
-
-			return ;
-
-		    }
-		    break;
-		default:
-		    _f();
-
+		function askQuestion(question) {
+			return new Promise((resolve) => {
+				interface.question(question, answer => resolve(answer) );
+			});
 		}
 
+		async function _f() {
 
-	    } catch(ex) {}
+			try {
 
-	};
+				let answer = await askQuestion(
+					`default todo location not found, use \"${DEFAULT}\" as default (y/n) `
+					), obj;
 
-	_f();
+				switch(answer) {
+					case 'y':
 
-    })();
+					interface.close();
+
+					obj = { location: DEFAULT, notification, timeout};
+
+					fs.writeFileSync(DEFAULT, "{}");
+
+					fs.writeFileSync("./config.json", JSON.stringify(obj));
+
+					module.exports = require('./duty.js')(DEFAULT);
+
+					break;
+					case 'n':
+
+					answer = await askQuestion(
+						`specify a location to save the information: `
+						);
+
+					interface.close();
+
+					if ( answer.length === 0 ) {
+						console.error(`invalid input specified\n`);
+						return ;
+					} else {
+
+						mkdirp.sync(answer.substr(0,answer.lastIndexOf(path.sep)));
+
+						obj = { location: answer + ".json", notification, timeout};
+
+						fs.writeFileSync(obj.location, "{}");
+
+						fs.writeFileSync("./config.json", JSON.stringify(obj));
+
+						module.exports = require('./duty.js')(answer);
+
+						return ;
+
+					}
+					break;
+					default:
+					_f();
+
+				}
+
+
+			} catch(ex) {}
+
+		};
+
+		_f();
+
+	})();
 }
