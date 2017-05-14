33747c9c6db383e7a8ef42c06a4c666299b43765
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-05-14 09:58:02 +0100

    rename `m` object to todoObj

--- deletetodo.js
+++ deletetodo.js
@@ -1,137 +1,137 @@
 class DeleteTodo {
-    constructor() {}
-    static createType() {
-	return new DeleteTodo();
-    }
-    handleDelete({type,opt,self: _this,DutyTodo}) {
+	constructor() {}
+	static createType() {
+		return new DeleteTodo();
+	}
+	handleDelete({type,opt,self: _this,DutyTodo}) {
 
-	let { m , location } = _this.MANAGER;
+		let { todoGroup , location } = _this.MANAGER;
 
-	this.type = type;
-	this.DutyTodo = DutyTodo;
-	this.m = m ;
-	this._this = _this;
-	this._opt = opt;
-	this.location = location;
+		this.type = type;
+		this.DutyTodo = DutyTodo;
+		this.todoGroup = todoGroup ;
+		this._this = _this;
+		this._opt = opt;
+		this.location = location;
 
-	this[this.type]();
-    }
-    all() {
+		this[this.type]();
+	}
+	all() {
 
-	let { DutyTodo,  _this: { MANAGER }, m , location} = this;
+		let { DutyTodo,  _this: { MANAGER }, todoGroup , location} = this;
 
-	delete MANAGER[m];
+		delete MANAGER[todoGroup];
 
-	m = {};
+		todoGroup = {};
 
-	DutyTodo.WriteFile({location,m});
+		DutyTodo.WriteFile({location,todoGroup});
 
-	return true;
-    }
-    hash() {
-	let { DutyTodo , location , m , _opt: { hash },_this} = this,
-	    hashRegex = new RegExp(`^${hash}`),
-	    j = 0,
-	    cb = ({longHash,hash}) => {
-		j++;
-		if ( hashRegex.test(longHash) ) {
-		    delete m[hash];
-		    return true;
-		} else if ( Object.keys(m).length === j ) {
-		    return false;
-		}
-	    };
+		return true;
+	}
+	hash() {
+		let { DutyTodo , location , todoGroup , _opt: { hash },_this} = this,
+		hashRegex = new RegExp(`^${hash}`),
+		j = 0,
+		cb = ({longHash,hash}) => {
+			j++;
+			if ( hashRegex.test(longHash) ) {
+				delete todoGroup[hash];
+				return true;
+			} else if ( Object.keys(todoGroup).length === j ) {
+				return false;
+			}
+		};
 
-	DutyTodo.CALLGENERATORYLOOP(_this,cb)
-	    .then( _ => {
-		DutyTodo.WriteFile({location,m});
-	    }).catch( _ => {
-		DutyTodo.ErrMessage(`${hash} was not found`);
-	    });
-    }
-    completed() {
-	let {DutyTodo,location,m,_this} = this,
+		DutyTodo.CALLGENERATORYLOOP(_this,cb)
+		.then( _ => {
+			DutyTodo.WriteFile({location,todoGroup});
+		}).catch( _ => {
+			DutyTodo.ErrMessage(`${hash} was not found`);
+		});
+	}
+	completed() {
+		let {DutyTodo,location,todoGroup,_this} = this,
 
-	    isDelete,j = 0,
+		isDelete,j = 0,
 
-	    cb = ({hash,completed}) => {
-		j++;
-		if ( completed ) {
-		    delete m[hash];
-		    isDelete = true;
-		    j--;
-		}
+		cb = ({hash,completed}) => {
+			j++;
+			if ( completed ) {
+				delete todoGroup[hash];
+				isDelete = true;
+				j--;
+			}
 
-		if ( ! isDelete && Object.keys(m).length === j ) {
-		    return false;
-		} else if ( isDelete && Object.keys(m).length === j ) {
-		    return true;
-		}
-	    };
+			if ( ! isDelete && Object.keys(todoGroup).length === j ) {
+				return false;
+			} else if ( isDelete && Object.keys(todoGroup).length === j ) {
+				return true;
+			}
+		};
 
-	DutyTodo.CALLGENERATORYLOOP(_this,cb)
-	    .then( _ => {
-		DutyTodo.WriteFile({location,m});
-		process.stdout.write(`completed todos have been deleted\n`);
-	    }).catch( _ => {
-		DutyTodo.ErrMessage(`Nothing was removed`);
-	    });
-    }
-    date() {
+		DutyTodo.CALLGENERATORYLOOP(_this,cb)
+		.then( _ => {
+			DutyTodo.WriteFile({location,todoGroup});
+			process.stdout.write(`completed todos have been deleted\n`);
+		}).catch( _ => {
+			DutyTodo.ErrMessage(`Nothing was removed`);
+		});
+	}
+	date() {
 
-	let { DutyTodo, _this, m, location} = this,
-	    { date: _userDate } = this._opt,
-	    isRead = false,j = 0,
-	    cb = ({date,modifiedDate,hash}) => {
-		j++;
-		if ( _userDate === date ) {
-		    delete m[hash];
-		    isRead = true;
-		    j--;
-		}
+		let { DutyTodo, _this, todoGroup, location} = this,
+		{ date: _userDate } = this._opt,
+		isRead = false,j = 0,
+		cb = ({date,modifiedDate,hash}) => {
+			j++;
+			if ( _userDate === date ) {
+				delete todoGroup[hash];
+				isRead = true;
+				j--;
+			}
 
-		if ( ! isRead && Object.keys(m).length === j ) {
-		    return false;
-		} else if ( isRead && Object.keys(m).length === j ) {
-		    return true;
-		}
-	    };
+			if ( ! isRead && Object.keys(todoGroup).length === j ) {
+				return false;
+			} else if ( isRead && Object.keys(todoGroup).length === j ) {
+				return true;
+			}
+		};
 
-	DutyTodo.CALLGENERATORYLOOP(_this,cb)
-	    .then( _ => {
-		DutyTodo.WriteFile({location,m});
-	    })
-	    .catch( _ => {
-		process.stdout.write(`no match for the specified date was found\n`);
-	    });
-    }
-    category() {
-	let { DutyTodo , location , m , _opt: { category },_this} = this,
-	    j = 0,isDelete = false,
-	    cb = ({hash,category: _category}) => {
+		DutyTodo.CALLGENERATORYLOOP(_this,cb)
+		.then( _ => {
+			DutyTodo.WriteFile({location,todoGroup});
+		})
+		.catch( _ => {
+			process.stdout.write(`no match for the specified date was found\n`);
+		});
+	}
+	category() {
+		let { DutyTodo , location , todoGroup , _opt: { category },_this} = this,
+		j = 0,isDelete = false,
+		cb = ({hash,category: _category}) => {
 
-		j++;
+			j++;
 
-		if ( _category && _category.includes(category) ) {
-		    delete m[hash];
-		    isDelete = true;
-		    j--;
-		}
+			if ( _category && _category.includes(category) ) {
+				delete todoGroup[hash];
+				isDelete = true;
+				j--;
+			}
 
-		if ( Object.keys(m).length === j && ( ! isDelete) ) {
-		    return false;
-		} else if ( Object.keys(m).length === j && ( isDelete ) ) {
-		    return true;
-		}
-	    };
+			if ( Object.keys(todoGroup).length === j && ( ! isDelete) ) {
+				return false;
+			} else if ( Object.keys(todoGroup).length === j && ( isDelete ) ) {
+				return true;
+			}
+		};
 
-	DutyTodo.CALLGENERATORYLOOP(_this,cb)
-	    .then( _ => {
-		DutyTodo.WriteFile({location,m});
-	    }).catch( _ => {
-		DutyTodo.ErrMessage(`${category} was not found`);
-	    });
-    }
+		DutyTodo.CALLGENERATORYLOOP(_this,cb)
+		.then( _ => {
+			DutyTodo.WriteFile({location,todoGroup});
+		}).catch( _ => {
+			DutyTodo.ErrMessage(`${category} was not found`);
+		});
+	}
 }
 
 module.exports = DeleteTodo;
--- readtodo.js
+++ readtodo.js
@@ -3,67 +3,67 @@
 //   the below class with DutyTodo class
 const colors = require('colors');
 class ReadTodo {
-    constructor() {}
-    static createType() {
-	return new ReadTodo();
-    }
-    static UNICODE_VALUES() {
-	const bold = colors.bold;
-	return {
-	    checkmark: bold("\u2714".green),
-	    ballot: bold("\u2718".red),
-	    critical: bold("\u25CF".red),
-	    notcritical: bold("\u25D0".green),
-	    critical: bold("\u2762".red),
-	    notcritical: bold("\u2762".green),
-	    circle: bold("\u25CF".red),
-	    halfcircle: bold('\u25CB'.green),
-	    completecircle: bold("\u25CF".green)
-	};
-    }
-    static HANDLE_DUE_DATE({due_date}) {
+	constructor() {}
+	static createType() {
+		return new ReadTodo();
+	}
+	static UNICODE_VALUES() {
+		const bold = colors.bold;
+		return {
+			checkmark: bold("\u2714".green),
+			ballot: bold("\u2718".red),
+			critical: bold("\u25CF".red),
+			notcritical: bold("\u25D0".green),
+			critical: bold("\u2762".red),
+			notcritical: bold("\u2762".green),
+			circle: bold("\u25CF".red),
+			halfcircle: bold('\u25CB'.green),
+			completecircle: bold("\u25CF".green)
+		};
+	}
+	static HANDLE_DUE_DATE({due_date}) {
 
-	let _date = new Date();
+		let _date = new Date();
 
-	_date = _date.toLocaleDateString().split('/').join('');
+		_date = _date.toLocaleDateString().split('/').join('');
 
-	due_date = due_date.split('/').join('');
+		due_date = due_date.split('/').join('');
 
-	const TIME_LEFT = String((due_date - _date)).replace(/0+$/,'');
+		const TIME_LEFT = String((due_date - _date)).replace(/0+$/,'');
 
-	const { circle, halfcircle, completecircle } = ReadTodo.UNICODE_VALUES();
+		const { circle, halfcircle, completecircle } = ReadTodo.UNICODE_VALUES();
 
-	if ( due_date > _date ) {
-	    return `${TIME_LEFT}days from now${halfcircle}`;
-	} else if ( due_date < _date ) {
-	    return `${parseInt(TIME_LEFT) * -1}days before now${circle}`;
-	} else if ( due_date === _date ) {
-	    return `today ${completecircle}`;
+		if ( due_date > _date ) {
+			return `${TIME_LEFT}days from now${halfcircle}`;
+		} else if ( due_date < _date ) {
+			return `${parseInt(TIME_LEFT) * -1}days before now${circle}`;
+		} else if ( due_date === _date ) {
+			return `today ${completecircle}`;
+		}
+
+	}
+	static HANDLE_PRIORITY(priority) {
+		return ((priority === 'critical') ?  'critical' : 'notcritical');
 	}
 
-    }
-    static HANDLE_PRIORITY(priority) {
-	return ((priority === 'critical') ?  'critical' : 'notcritical');
-    }
+	static STYLE_READ(opt,DutyTodo) {
 
-    static STYLE_READ(opt,DutyTodo) {
+		let {
+			hash,
+			content,
+			completed,
+			date,
+			modifiedDate,
+			due_date,
+			priority,
+			urgency,
+			category,
+			note
+		} = opt;
 
-	let {
-	    hash,
-	    content,
-	    completed,
-	    date,
-	    modifiedDate,
-	    due_date,
-	    priority,
-	    urgency,
-	    category,
-	    note
-	} = opt;
+		let unicodes = ReadTodo.UNICODE_VALUES();
 
-	let unicodes = ReadTodo.UNICODE_VALUES();
-
-	DutyTodo.PRINT(`
+		DutyTodo.PRINT(`
 
 hash:\t\t${hash}  ${completed ? unicodes.checkmark : unicodes.ballot}
 creation date:\t${date} ${modifiedDate ? `
@@ -74,203 +74,204 @@ priority:\t${priority}${unicodes[ReadTodo.HANDLE_PRIORITY(priority)]}` : ''} ${u
 urgency:\t${urgency} `: ''} ${note ? `
 note:\t\t${note}`: ''}
 content:\t${content}\n
+
 `);
 
-    }
-    handleRead({type,opt,self: _this,DutyTodo}) {
-	let { m } = _this.MANAGER;
-	this.type = type;
-	this.DutyTodo = DutyTodo;
-	this.m = m ;
-	this._this = _this;
-	this._opt = opt;
-
-	let _matched = this.type.match(/^(urgency|category):([a-z]+)$/);
-	const [,_type,_typeOfType] = _matched ? _matched : [,undefined,undefined];
-
-	if ( _typeOfType ) {
-	    this[_type](_typeOfType);
-	    return ;
 	}
+	handleRead({type,opt,self: _this,DutyTodo}) {
+		let { todoGroup } = _this.MANAGER;
+		this.type = type;
+		this.DutyTodo = DutyTodo;
+		this.todoGroup = todoGroup ;
+		this._this = _this;
+		this._opt = opt;
 
-	this[this.type]();
-    }
-    all() {
+		let _matched = this.type.match(/^(urgency|category):([a-z]+)$/);
+		const [,_type,_typeOfType] = _matched ? _matched : [,undefined,undefined];
 
-	let { DutyTodo, _this, m } = this;
-
-	DutyTodo.CALLGENERATORYLOOP(_this, ({hash}) => {
-	    ReadTodo.STYLE_READ(m[hash],DutyTodo);
-	});
-    }
-    due() {
-
-	let { DutyTodo, _this, m } = this,
-	    { date: _dueDate,_cb} = this._opt, j = 0,
-	    cb = ({hash,due_date}) => {
-
-		j++;
-		if ( due_date && _dueDate === due_date ) {
-                    if ( _cb ) {
-                        _cb(m[hash]);
-                        return DutyTodo.DAEMONMATCH;
-                    }
-
-		    ReadTodo.STYLE_READ(m[hash],DutyTodo);
-		    return true;
+		if ( _typeOfType ) {
+			this[_type](_typeOfType);
+			return ;
 		}
-		if ( Object.keys(m).length === j ) {
 
-                    if ( ! _cb ) return false;
+		this[this.type]();
+	}
+	all() {
 
-                    return DutyTodo.NO_DAEMONMATCH;
-		}
-	    };
+		let { DutyTodo, _this, todoGroup } = this;
+
+		DutyTodo.CALLGENERATORYLOOP(_this, ({hash}) => {
+			ReadTodo.STYLE_READ(todoGroup[hash],DutyTodo);
+		});
+	}
+	due() {
+
+		let { DutyTodo, _this, todoGroup } = this,
+		{ date: _dueDate,_cb} = this._opt, j = 0,
+		cb = ({hash,due_date}) => {
+
+			j++;
+			if ( due_date && _dueDate === due_date ) {
+				if ( _cb ) {
+					_cb(todoGroup[hash]);
+					return DutyTodo.DAEMONMATCH;
+				}
+
+				ReadTodo.STYLE_READ(todoGroup[hash],DutyTodo);
+				return true;
+			}
+			if ( Object.keys(todoGroup).length === j ) {
+
+				if ( ! _cb ) return false;
+
+				return DutyTodo.NO_DAEMONMATCH;
+			}
+		};
 
         // the catch block is entirely useless
         //   when due is executed from the daemon
         //   an extra check will be made to see if a due date is set
 
-	DutyTodo.CALLGENERATORYLOOP(_this,cb)
-	    .catch( _ => {
-		process.stdout.write(`specified due date was not found\n`);
-	    });
+        DutyTodo.CALLGENERATORYLOOP(_this,cb)
+        .catch( _ => {
+        	process.stdout.write(`specified due date was not found\n`);
+        });
 
+      }
+      category(categoryType) {
+
+      	let { DutyTodo, _this, todoGroup } = this,
+      	isRead = false, j = 0,
+      	cb = ({hash,category}) => {
+      		j++;
+      		if ( category && Array.isArray(category) && category.includes(categoryType)) {
+      			isRead = true;
+
+      			ReadTodo.STYLE_READ(todoGroup[hash],DutyTodo);
+      		}
+
+      		if ( ! isRead && Object.keys(todoGroup).length === j ) {
+      			return false;
+      		} else if ( isRead && Object.keys(todoGroup).length === j ) {
+      			return true;
+      		}
+      	};
+
+      	DutyTodo.CALLGENERATORYLOOP(_this,cb)
+      	.catch( _ => {
+      		process.stdout.write(`no todo with such category\n`);
+      	});
+      }
+      urgency(urgencyType) {
+
+      	let { DutyTodo, _this, todoGroup } = this;
+      	switch(urgencyType) {
+      		case "pending":break;
+      		case "waiting":break;
+      		case "tomorrow":break;
+      		case "later":break;
+      		case "today": break;
+      		default:
+      		DutyTodo.ErrMessage(`invalid urgency type to read`);
+      		return false;
+      	}
+
+      	let isRead = false, j = 0,
+      	cb = ({hash,urgency}) => {
+      		j++;
+      		if ( urgency && Array.isArray(urgency) && urgency.includes(urgencyType) ) {
+      			isRead = true;
+      			ReadTodo.STYLE_READ(todoGroup[hash],DutyTodo);
+      		}
+
+      		if ( ! isRead && Object.keys(todoGroup).length === j ) {
+      			return false;
+      		} else if ( isRead && Object.keys(todoGroup).length === j ) {
+      			return true;
+      		}
+      	};
+
+      	DutyTodo.CALLGENERATORYLOOP(_this,cb)
+      	.catch( _ => {
+      		process.stdout.write(`no todo with such urgency\n`);
+      	});
+
+      }
+      completed() {
+      	let { DutyTodo, _this, todoGroup } = this,
+      	isRead = false,j = 0,
+      	cb = ({completed,hash}) => {
+      		j++;
+      		if ( completed ) {
+      			isRead = true;
+      			ReadTodo.STYLE_READ(todoGroup[hash],DutyTodo);
+      		}
+
+      		if ( ! isRead && Object.keys(todoGroup).length === j ) {
+      			return false;
+      		} else if ( isRead && Object.keys(todoGroup).length === j ) {
+      			return true;
+      		}
+      	};
+
+      	DutyTodo.CALLGENERATORYLOOP(_this,cb)
+      	.catch( _ => {
+      		process.stdout.write(`nothing complete to read\n`);
+      	});
+
+      }
+      notcompleted() {
+
+      	let { DutyTodo, _this, todoGroup } = this,
+      	isRead = false,j = 0,
+      	cb = ({completed,hash}) => {
+      		j++;
+      		if ( ! completed ) {
+      			isRead = true;
+      			ReadTodo.STYLE_READ(todoGroup[hash],DutyTodo);
+      		}
+
+      		if ( ! isRead && Object.keys(todoGroup).length === j ) {
+      			return false;
+      		} else if ( isRead && Object.keys(todoGroup).length === j ) {
+      			return true;
+      		}
+      	};
+
+      	DutyTodo.CALLGENERATORYLOOP(_this,cb)
+      	.catch( _ => {
+      		process.stdout.write(`nothing complete to read\n`);
+      	});
+
+      }
+      date() {
+      	let { DutyTodo, _this, todoGroup } = this,
+      	{ date: _userDate , modifiedDate: _userModifiedDate} = this._opt,
+      	isRead = false,j = 0,
+      	cb = ({date,modifiedDate,hash}) => {
+      		j++;
+      		if ( (_userDate && date === _userDate) && (_userModifiedDate && modifiedDate === _userModifiedDate)
+      			) {
+      			isRead = true;
+      		ReadTodo.STYLE_READ(todoGroup[hash],DutyTodo);
+      	} else if ( (_userDate && date === _userDate) && !_userModifiedDate ) {
+      		isRead = true;
+      		ReadTodo.STYLE_READ(todoGroup[hash],DutyTodo);
+      	}
+
+      	if ( ! isRead && Object.keys(todoGroup).length === j ) {
+      		return false;
+      	} else if ( isRead && Object.keys(todoGroup).length === j ) {
+      		return true;
+      	}
+      };
+
+      DutyTodo.CALLGENERATORYLOOP(_this,cb)
+      .catch( _ => {
+      	process.stdout.write(`no match for the specified date was found\n`);
+      });
     }
-    category(categoryType) {
-
-	let { DutyTodo, _this, m } = this,
-	    isRead = false, j = 0,
-	    cb = ({hash,category}) => {
-		j++;
-		if ( category && Array.isArray(category) && category.includes(categoryType)) {
-		    isRead = true;
-
-		    ReadTodo.STYLE_READ(m[hash],DutyTodo);
-		}
-
-		if ( ! isRead && Object.keys(m).length === j ) {
-		    return false;
-		} else if ( isRead && Object.keys(m).length === j ) {
-		    return true;
-		}
-	    };
-
-	DutyTodo.CALLGENERATORYLOOP(_this,cb)
-	    .catch( _ => {
-		process.stdout.write(`no todo with such category\n`);
-	    });
-    }
-    urgency(urgencyType) {
-
-	let { DutyTodo, _this, m } = this;
-	switch(urgencyType) {
-	case "pending":break;
-	case "waiting":break;
-	case "tomorrow":break;
-	case "later":break;
-	case "today": break;
-	default:
-	    DutyTodo.ErrMessage(`invalid urgency type to read`);
-	    return false;
-	}
-
-	let isRead = false, j = 0,
-	    cb = ({hash,urgency}) => {
-		j++;
-		if ( urgency && Array.isArray(urgency) && urgency.includes(urgencyType) ) {
-		    isRead = true;
-		    ReadTodo.STYLE_READ(m[hash],DutyTodo);
-		}
-
-		if ( ! isRead && Object.keys(m).length === j ) {
-		    return false;
-		} else if ( isRead && Object.keys(m).length === j ) {
-		    return true;
-		}
-	    };
-
-	DutyTodo.CALLGENERATORYLOOP(_this,cb)
-	    .catch( _ => {
-		process.stdout.write(`no todo with such urgency\n`);
-	    });
-
-    }
-    completed() {
-	let { DutyTodo, _this, m } = this,
-	    isRead = false,j = 0,
-	    cb = ({completed,hash}) => {
-		j++;
-		if ( completed ) {
-		    isRead = true;
-		    ReadTodo.STYLE_READ(m[hash],DutyTodo);
-		}
-
-		if ( ! isRead && Object.keys(m).length === j ) {
-		    return false;
-		} else if ( isRead && Object.keys(m).length === j ) {
-		    return true;
-		}
-	    };
-
-	DutyTodo.CALLGENERATORYLOOP(_this,cb)
-	    .catch( _ => {
-		process.stdout.write(`nothing complete to read\n`);
-	    });
-
-    }
-    notcompleted() {
-
-	let { DutyTodo, _this, m } = this,
-	    isRead = false,j = 0,
-	    cb = ({completed,hash}) => {
-		j++;
-		if ( ! completed ) {
-		    isRead = true;
-		    ReadTodo.STYLE_READ(m[hash],DutyTodo);
-		}
-
-		if ( ! isRead && Object.keys(m).length === j ) {
-		    return false;
-		} else if ( isRead && Object.keys(m).length === j ) {
-		    return true;
-		}
-	    };
-
-	DutyTodo.CALLGENERATORYLOOP(_this,cb)
-	    .catch( _ => {
-		process.stdout.write(`nothing complete to read\n`);
-	    });
-
-    }
-    date() {
-	let { DutyTodo, _this, m } = this,
-	    { date: _userDate , modifiedDate: _userModifiedDate} = this._opt,
-	    isRead = false,j = 0,
-	    cb = ({date,modifiedDate,hash}) => {
-		j++;
-		if ( (_userDate && date === _userDate) && (_userModifiedDate && modifiedDate === _userModifiedDate)
-		   ) {
-		    isRead = true;
-		    ReadTodo.STYLE_READ(m[hash],DutyTodo);
-		} else if ( (_userDate && date === _userDate) && !_userModifiedDate ) {
-		    isRead = true;
-		    ReadTodo.STYLE_READ(m[hash],DutyTodo);
-		}
-
-		if ( ! isRead && Object.keys(m).length === j ) {
-		    return false;
-		} else if ( isRead && Object.keys(m).length === j ) {
-		    return true;
-		}
-	    };
-
-	DutyTodo.CALLGENERATORYLOOP(_this,cb)
-	    .catch( _ => {
-		process.stdout.write(`no match for the specified date was found\n`);
-	    });
-    }
 
 
-}
-module.exports = ReadTodo;
+  }
+  module.exports = ReadTodo;
