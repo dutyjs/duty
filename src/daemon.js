df835777389ea1ddfc8e6767510867840822b53a
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-06-11 04:05:08 +0100

    add support for daemon notification for osx

--- src/daemon.js
+++ src/daemon.js
@@ -1,23 +1,30 @@
 const fs = require("fs")
 const cp = require("child_process")
+const path = require('path');
 
 const { LINUX , WINDOWS, DARWIN } = require("./daemonconfig")
 
 const FILE_MAP = new Map([
     ["windows", "NA"],
-    ["linux", "/etc/systemd/system/duty-js.service"],
-    ["darwin", "NA"]
-])
+    ["linux", `${path.join(path.sep, "etc", "systemd", "system", "duty-js.service")}`],
+    ["darwin", `${path.join(process.env.HOME, "Library", "LaunchAgents", "com.duty.js.plist")}`]
+]);
 
 let configBuild = ""
 
-const flatten = (value = {}) => {
-    if ( typeof(value) === "object" && ! Array.isArray(value) && Object.keys(value).length != 0 ) {
+const isObject = value => {
+    return typeof(value) === "object" && ! Array.isArray(value);
+};
+
+const flattenSYSTEMD = (value = {}) => {
+    if ( isObject(value) ) {
         for ( let [key,_value] of Object.entries(value) ) {
             configBuild += `\n[${key}]\n`
             setupValues(_value)
         }
-        return configBuild
+        let _Cbuild = configBuild;
+        configBuild = "";
+        return _Cbuild;
     }
 }
 
@@ -27,9 +34,16 @@ const setupValues = (value) => {
     }
 }
 
-module.exports = class Daemon {
+class Daemon {
     static CreateDaemon(platform) {
-        Daemon[platform]()
+        // every things that happens in this
+        //  static method is just to make the code,
+        //  dependency injectable
+
+        let { writeConfig: wr } = Daemon,
+            { execSync } = cp;
+
+        Daemon[platform]({wr,execSync});
     }
     static writeConfig({location,config}) {
         try {
@@ -38,20 +52,48 @@ module.exports = class Daemon {
             throw ex
         }
     }
-    static linux() {
-        const config = flatten(LINUX)
+    static wait() {
+        process.stdout.write("wait while the daemon is been enabled...")        
+    }
+    static linux({wr,execSync}) {
+        const config = flattenSYSTEMD(LINUX)
         const location = FILE_MAP.get("linux")
 
-        Daemon.writeConfig({location,config})
-        process.stdout.write("wait while the daemon is been enabled...")
-        
-        cp.execSync("systemctl enable duty-js && systemctl daemon-reload && systemctl start duty-js")
+        wr({location,config})
+
+        Daemon.wait();
+        try {
+            execSync("systemctl enable duty-js && systemctl daemon-reload && systemctl start duty-js")
+        } catch(ex) {
+            throw(`error encountered while enabling service`);
+        }
+
+        return true;
+    }
+    static windows({wr,execSync}) {
+        // 
+    }
+    static darwin({wr,execSync}) {
+
+        const config = DARWIN;
+        const location = FILE_MAP.get("darwin");
+
+        wr({location,DARWIN});
+
+        Daemon.wait();
+
+        try {
+            execSync(`launchctl load -w ${location} && launchctl start com.duty.js`);
+        } catch(ex) {
+            throw(`error encountered while enabling service`);
+        }
+
+        return true;
 
     }
-    static windows() {
+}
 
-    }
-    static darwin() {
-
-    }
+module.exports = {
+    Daemon,
+    FILE_MAP
 }
