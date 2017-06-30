9323ca855bfa84cb03cf86f48ff7c1f30efb8f78
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-06-30 01:58:40 +0100

    linting for daemon.js

--- src/daemon.js
+++ src/daemon.js
@@ -2,34 +2,33 @@ const fs = require("fs");
 const cp = require("child_process");
 
 module.exports = class Daemon {
-    static CreateDaemon(platform) {
-        // every things that happens in this
-        //  static method is just to make the code,
-        //  dependency injectable
+  static CreateDaemon (platform) {
+    // every things that happens in this
+    //  static method is just to make the code,
+    //  dependency injectable
 
-        return Daemon[platform]();
+    return Daemon[platform]();
+  }
+  static wait () {
+    process.stdout.write("wait while the daemon is been enabled...");
+  }
+  static execSetupScript () {
+    Daemon.wait();
+    try {
+      cp.execSync(`${__dirname}/daemon.bash duty`);
+    } catch (ex) {
+      throw ("error encountered while enabling service");
     }
-    static wait() {
-        process.stdout.write("wait while the daemon is been enabled...");        
-    }
-    static execSetupScript() {
-        Daemon.wait();
-        try {
-            cp.execSync(`${__dirname}/daemon.bash duty`);
-        } catch(ex) {
-            throw("error encountered while enabling service");
-        }
 
-        return Promise.resolve("daemon has been created");
-
-    }
-    static linux() {
-        return Daemon.execSetupScript();
-    }
-    static windows() {
-        // return Daemon.execSetupScript();
-    }
-    static darwin() {
-        return Daemon.execSetupScript();
-    }
+    return Promise.resolve("daemon has been created");
+  }
+  static linux () {
+    return Daemon.execSetupScript();
+  }
+  static windows () {
+    // return Daemon.execSetupScript();
+  }
+  static darwin () {
+    return Daemon.execSetupScript();
+  }
 };
