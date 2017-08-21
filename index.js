afa20d332e2d4634e8bd7528cc5681504596d656
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-08-21 16:23:17 +0100

    Reading todo from environmental variable
    Add support for reading todo location from DUTY_TODO environmental
    varialble

--- index.js
+++ index.js
@@ -8,111 +8,126 @@ const confLocation = path.join(homedir(), ".duty.config.json");
 if ( ! fs.existsSync(confLocation) ) fs.writeFileSync(confLocation,"{}");
 
 function isCFGLocation(location) {
-  return fs.existsSync(location);
+    return fs.existsSync(location);
 }
 
 try {
 
-  const config = require(confLocation);
-  Object.assign(config, {
-    todoGroup: require(config.location)
-  });
+    const config = require(confLocation);
+    
+    const envLocation = process.env.DUTY_TODO;
+    
+    if ( envLocation && fs.existsSync(envLocation) ) {
 
-  module.exports = new(require("./src/duty.js"))(config);
+        config.location = envLocation;
+        
+    } else if ( envLocation && ! fs.existsSync(envLocation) ) {
+        
+        fs.writeFileSync(envLocation, JSON.stringify({}));
+
+        config.location = envLocation;
+        
+    }
+
+    config.todoGroup = require(config.location);
+
+
+    module.exports = new(require("./src/duty.js"))(config);
 
 
 } catch (ex) {
-  // if ( process.env.NODE_ENV === "development" ) {
-  //     console.log(ex);
-  // }
-  ;
-  (() => {
-    const rl = require("readline");
-    const DEFAULT = `${path.join(homedir(), ".duty.json")}`;
-    
-    const _interface = rl.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
+    // if ( process.env.NODE_ENV === "development" ) {
+    //     console.log(ex);
+    // }
+    console.log(ex);
+    ;
+    (() => {
+        const rl = require("readline");
+        const DEFAULT = `${path.join(homedir(), ".duty.json")}`;
+        
+        const _interface = rl.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
-    function askQuestion(question) {
-      return new Promise((resolve) => {
-        _interface.question(question, answer => resolve(answer));
-      });
-    }
-
-    async function _f() {
-
-      try {
-
-        let answer = await askQuestion(
-            `default todo location not found, use \"${DEFAULT}\" as default (y/n) `
-          ),
-          config;
-
-
-        switch (answer) {
-        case "y":
-
-          _interface.close();
-
-          config = {
-            location: DEFAULT
-          };
-
-          if ( ! isCFGLocation(config.location) )
-            fs.writeFileSync(config.location, "{}");
-
-          fs.writeFileSync(confLocation, JSON.stringify(config));
-
-          module.exports = new(require("./src/duty.js"))(config);
-
-          break;
-        case "n":
-
-          answer = await askQuestion(
-            "specify a directory to save the todos "
-          );
-
-          _interface.close();
-
-          if (answer.length === 0) {
-            console.error("invalid input specified\n");
-            break;
-          }
-
-          // mkdirp.sync(answer.substr(0, answer.lastIndexOf(path.sep)));
-
-          if ( ! fs.existsSync(answer) ) {
-            console.error(`${answer} does not exists`);
-            return ;
-          } else {
-            config = {
-              location: `${path.join(answer, ".duty.json")}`
-            };
-          }
-
-          if ( ! isCFGLocation(config.location) )
-            fs.writeFileSync(config.location, "{}");
-                    
-          fs.writeFileSync(confLocation, JSON.stringify(config));
-
-          module.exports = new(require("./src/duty.js"))(config);
-
-          break;
-        default:
-          _f();
-
+        function askQuestion(question) {
+            return new Promise((resolve) => {
+                _interface.question(question, answer => resolve(answer));
+            });
         }
 
+        async function _f() {
 
-      } catch (ex) {
-        console.log(ex);
-      }
+            try {
 
-    };
+                let answer = await askQuestion(
+                    `default todo location not found, use \"${DEFAULT}\" as default (y/n) `
+                ),
+                    config;
 
-    _f();
 
-  })();
+                switch (answer) {
+                case "y":
+
+                    _interface.close();
+
+                    config = {
+                        location: DEFAULT
+                    };
+
+                    if ( ! isCFGLocation(config.location) )
+                        fs.writeFileSync(config.location, "{}");
+
+                    fs.writeFileSync(confLocation, JSON.stringify(config));
+
+                    module.exports = new(require("./src/duty.js"))(config);
+
+                    break;
+                case "n":
+
+                    answer = await askQuestion(
+                        "specify a directory to save the todos "
+                    );
+
+                    _interface.close();
+
+                    if (answer.length === 0) {
+                        console.error("invalid input specified\n");
+                        break;
+                    }
+
+                    // mkdirp.sync(answer.substr(0, answer.lastIndexOf(path.sep)));
+
+                    if ( ! fs.existsSync(answer) ) {
+                        console.error(`${answer} does not exists`);
+                        return ;
+                    } else {
+                        config = {
+                            location: `${path.join(answer, ".duty.json")}`
+                        };
+                    }
+
+                    if ( ! isCFGLocation(config.location) )
+                        fs.writeFileSync(config.location, "{}");
+                    
+                    fs.writeFileSync(confLocation, JSON.stringify(config));
+
+                    module.exports = new(require("./src/duty.js"))(config);
+
+                    break;
+                default:
+                    _f();
+
+                }
+
+
+            } catch (ex) {
+                console.log(ex);
+            }
+
+        };
+
+        _f();
+
+    })();
 }
